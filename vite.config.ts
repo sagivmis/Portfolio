c7ea853d50113a523a0bb4dc76762598c06817f3
sagivmis <sagiv505@gmail.com>
2024-05-22 13:24:24 +0300

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import path from "path"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    emptyOutDir: true,
+    outDir: "dist",
+    // Additional configuration to make sure Vite works well with Electron
+    rollupOptions: {
+      output: {
+        format: "cjs"
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    }
+  }
 })
