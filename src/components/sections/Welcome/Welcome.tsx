320dbec7b376bc76081aa0b45d2efbe1b2de12af
sagivmis <sagiv505@gmail.com>
2024-06-05 19:12:46 +0300

    implemented welcome page (WIP)

--- src/components/sections/Welcome/Welcome.tsx
+++ src/components/sections/Welcome/Welcome.tsx
@@ -1,15 +1,42 @@
-import React from "react"
+import React, { CSSProperties, useEffect, useMemo, useState } from "react"
 import "./welcome.css"
 import { ISection } from "../../../types"
 import SketchComponent from "./Sketch/Sketch"
+import clsx from "clsx"
 
 interface IWelcome extends ISection {}
 const Welcome = (props: IWelcome) => {
-  const { className } = props
+  const { className, currentSection } = props
+  const [style, setStyle] = useState<CSSProperties>()
+
+  const isCurrentSection = useMemo(
+    () => currentSection === "welcome" || currentSection === "",
+    [currentSection]
+  )
+
+  useEffect(() => {
+    if (!isCurrentSection)
+      setTimeout(() => !isCurrentSection && setStyle({ display: "none" }), 700)
+    // ms of delay should be equal to full page delay
+    else setTimeout(() => isCurrentSection && setStyle(undefined), 700 / 3)
+  }, [currentSection, isCurrentSection])
   return (
-    <section className='welcome-container'>
+    <section className={clsx("welcome-container", className)} style={style}>
       <div className='welcome-content-container'>
-        {/* <SketchComponent /> */}
+        <div className='intro'>
+          <div className='upper'>
+            <h4 className='welcome-title'>Hello, I'm</h4>
+            <div className='border-anim-container'>
+              <a href='#about' className='border-animation'>
+                <div className='border-animation__inner'>Sagiv Mishaan</div>
+              </a>
+            </div>{" "}
+            <h4>.</h4>
+          </div>
+          <div className='lower'>
+            <h4 className='welcome-subtitle'>I'm a full-stack web developer</h4>
+          </div>
+        </div>
       </div>
     </section>
   )
