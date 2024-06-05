1c062da800afb20123ee974c603435eae9c8d407
sagivmis <sagiv505@gmail.com>
2024-06-05 19:12:24 +0300

    implemented projects section

--- src/components/sections/Projects/Project/Project.tsx
+++ src/components/sections/Projects/Project/Project.tsx
@@ -1,18 +1,77 @@
-import { ISection, ProjectType } from "../../../../types"
+import clsx from "clsx"
+import { Skeleton } from "../../../../assets"
+import { ProjectType } from "../../../../types"
 import "./project.css"
+import {
+  Dispatch,
+  SetStateAction,
+  memo,
+  useCallback,
+  useMemo,
+  useRef
+} from "react"
+import useOnClickOutside from "../../../../hooks/useOnClickOutside"
 
-interface IProject extends ISection {
+interface IProject {
   project: ProjectType
+  selected: boolean
+  index: number
+  changeSelection: Dispatch<SetStateAction<number>>
+  onClick?: () => void
+  removeSelection: () => void
 }
 
 const Project = (props: IProject) => {
-  const { project } = props
+  const {
+    project,
+    selected,
+    index,
+    removeSelection,
+    onClick,
+    changeSelection
+  } = props
+
+  const projectRef = useRef<HTMLDivElement>(null)
+  const isSelected = useMemo(() => selected, [selected])
+
+  const handleRemoveSelection = useCallback(() => {
+    if (isSelected) {
+      removeSelection()
+    }
+  }, [removeSelection, isSelected])
+  useOnClickOutside(projectRef, handleRemoveSelection)
+
   return (
-    <div className='project'>
-      <h3 className='project-title'>{project.title}</h3>
-      <p className='project-content'>{project.content}</p>
+    <div
+      className={clsx("project", { selected: isSelected })}
+      onClick={
+        !isSelected
+          ? () => {
+              changeSelection(index)
+              onClick()
+            }
+          : () => {
+              removeSelection()
+              onClick()
+            }
+      }
+      ref={projectRef}
+    >
+      <div className='project-info'>
+        <h3 className='project-title'>{project.title}</h3>
+        <p className='project-content'>{isSelected && project.content}</p>
+      </div>
+      <div className='project-images-container'>
+        {isSelected && (
+          <img
+            className='project-image'
+            src={project.images ? project.images.img1 : Skeleton}
+            alt='example'
+          />
+        )}
+      </div>
     </div>
   )
 }
 
-export default Project
+export default memo(Project)
--- src/components/sections/Projects/Projects.tsx
+++ src/components/sections/Projects/Projects.tsx
@@ -1,35 +1,78 @@
 import clsx from "clsx"
 import "./projects.css"
-import { ISection, ProjectType } from "../../../types"
+import { ISection } from "../../../types"
 import Project from "./Project/Project"
+import {
+  CSSProperties,
+  memo,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState
+} from "react"
+import { projects } from "../../../util/consts"
 
 interface IProjects extends ISection {}
 
-const projects: ProjectType[] = [
-  {
-    title: "Cryptocurrency Algo-Trade Bot",
-    content:
-      "Developed using Python with pandas and numpy for the backend and\n React with Electron for the front-end."
-  },
-  {
-    title: "AI Chatbot",
-    content: "with dashboard for user preferences and file management."
-  },
-  {
-    title: "Independent Game Development",
-    content: "Created 2D and 3D arcade games with AI systems for NPCs."
-  }
-]
-
 const Projects = (props: IProjects) => {
-  const { className } = props
+  const { currentSection, className } = props
+  const [selectedProjectIndex, setSelectedProjectIndex] = useState(-1)
+  const [style, setStyle] = useState<CSSProperties>()
+
+  const isCurrentSection = useMemo(
+    () => currentSection === "projects",
+    [currentSection]
+  )
+
+  const handleResetSelectedProject = useCallback(() => {
+    setSelectedProjectIndex(-1)
+  }, [])
+
+  useEffect(() => {
+    if (!isCurrentSection)
+      setTimeout(() => !isCurrentSection && setStyle({ display: "none" }), 700)
+    // ms of delay should be equal to full page delay
+    else setTimeout(() => isCurrentSection && setStyle(undefined), 700 / 3)
+  }, [currentSection, isCurrentSection])
+
   return (
-    <section className={clsx("projects-container", className)}>
-      {projects.map((project) => (
-        <Project project={project} />
-      ))}
+    <section className={clsx("projects-container", className)} style={style}>
+      {projects.map(
+        (project, index) =>
+          index % 3 === 0 && (
+            <div className='slide'>
+              {projects[index] && (
+                <Project
+                  project={projects[index]}
+                  selected={selectedProjectIndex === index}
+                  removeSelection={handleResetSelectedProject}
+                  index={index}
+                  changeSelection={setSelectedProjectIndex}
+                />
+              )}
+              {projects[index + 1] && (
+                <Project
+                  project={projects[index + 1]}
+                  selected={selectedProjectIndex === index + 1}
+                  removeSelection={handleResetSelectedProject}
+                  index={index + 1}
+                  changeSelection={setSelectedProjectIndex}
+                />
+              )}
+              {projects[index + 2] && (
+                <Project
+                  project={projects[index + 2]}
+                  selected={selectedProjectIndex === index + 2}
+                  removeSelection={handleResetSelectedProject}
+                  index={index + 2}
+                  changeSelection={setSelectedProjectIndex}
+                />
+              )}
+            </div>
+          )
+      )}
     </section>
   )
 }
 
-export default Projects
+export default memo(Projects)
