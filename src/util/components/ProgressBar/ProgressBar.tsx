a9df4fcf7a543fe76c922ee11cd9ec5ebde36a2d
Sagiv Mishaan <sagiv505@gmail.com>
2025-03-08 12:15:49 +0200

    css fixes

--- src/util/components/ProgressBar/ProgressBar.tsx
+++ src/util/components/ProgressBar/ProgressBar.tsx
@@ -1,40 +1,38 @@
-import { useEffect, useMemo, useState } from "react"
-import "./progress-bar.css"
+import { useEffect, useMemo, useState } from "react";
+import "./progress-bar.css";
 
 interface IProgressBar {
-  value: number
-  max?: number
-  label?: string
-  animationDelay?: number
+  value: number;
+  max?: number;
+  label?: string;
+  animationDelay?: number;
 }
 const ProgressBar = (props: IProgressBar) => {
-  const { label, max = 100, value, animationDelay = 0 } = props
-  const percentage = useMemo(() => (value / max) * 100, [max, value])
+  const { label, max = 100, value, animationDelay = 0 } = props;
+  const percentage = useMemo(() => (value / max) * 100, [max, value]);
 
-  const [progress, setProgress] = useState(0)
+  const [progress, setProgress] = useState(0);
 
   useEffect(() => {
-    setTimeout(() => setProgress(percentage), 1000)
-    return () => setProgress(0)
-  }, [percentage])
+    setTimeout(() => setProgress(percentage), 1000);
+    return () => setProgress(0);
+  }, [percentage]);
 
   return (
-    <div className='skill-progress-container'>
-      {label && <div className='skill-name'>{label}</div>}
-      <div className='progress-bar-container'>
+    <div className="skill-progress-container">
+      {label && <div className="skill-name">{label}</div>}
+      <div className="progress-bar-container">
         <div
-          className='progress-bar'
+          className="progress-bar"
           style={{
             inlineSize: `${progress}%`,
             transitionDelay: `${350 + animationDelay}ms`,
-            transitionDuration: `${1000 - animationDelay}ms`
+            transitionDuration: `${1000 - animationDelay}ms`,
           }}
-        >
-          {/* {percentage}% */}
-        </div>
+        ></div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProgressBar
+export default ProgressBar;
