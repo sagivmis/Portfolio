f8f766562aeffc4467a5ba746b5d99e127efdaec
Sagiv Mishaan <sagiv505@gmail.com>
2025-03-08 13:56:17 +0200

    change links color

--- src/components/Header/LinkBar/LinkBar.tsx
+++ src/components/Header/LinkBar/LinkBar.tsx
@@ -1,50 +1,40 @@
-import { CSSProperties, useEffect, useState } from "react"
-import { SectionNames } from "../../../types"
-import "./link-bar.css"
+import { CSSProperties, useEffect, useState } from "react";
+import { SectionNames } from "../../../types";
+import "./link-bar.css";
 
 interface ILinkBar {
-  currentSection: SectionNames
+  currentSection: SectionNames;
 }
 const LinkBar = (props: ILinkBar) => {
-  const { currentSection } = props
-  const [linkStyle, setLinkStyle] = useState<CSSProperties>()
+  const { currentSection } = props;
+  const [linkStyle, setLinkStyle] = useState<CSSProperties>();
 
-  useEffect(() => {
-    if (currentSection === "about")
-      setLinkStyle({
-        color: "var(--background)"
-      })
-    else
-      setLinkStyle({
-        color: "black"
-      })
-  }, [currentSection])
   return (
-    <nav className='link-bar-container'>
-      <ul className='link-list'>
-        <li className='link'>
-          <a href='#about' style={linkStyle}>
+    <nav className="link-bar-container">
+      <ul className="link-list">
+        <li className="link">
+          <a href="#about" style={linkStyle}>
             About
           </a>
         </li>
-        <li className='link'>
-          <a href='#experience' style={linkStyle}>
+        <li className="link">
+          <a href="#experience" style={linkStyle}>
             Experience
           </a>
         </li>
-        <li className='link'>
-          <a href='#projects' style={linkStyle}>
+        <li className="link">
+          <a href="#projects" style={linkStyle}>
             Projects
           </a>
         </li>
-        <li className='link'>
-          <a href='#contact' style={linkStyle}>
+        <li className="link">
+          <a href="#contact" style={linkStyle}>
             Contact
           </a>
         </li>
       </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default LinkBar
+export default LinkBar;
